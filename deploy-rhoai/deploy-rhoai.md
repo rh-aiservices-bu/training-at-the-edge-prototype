a11e7ebccabc76ed17b3b0353bb8d0a3855ffe82
Erwan Granger <32554862+erwangranger@users.noreply.github.com>
2023-10-31 22:06:29 -0400

    adding some initial content

---
+++ deploy-rhoai/deploy-rhoai.md
@@ -0,0 +1,71 @@
+
+
+
+```bash
+cat <<EOF | oc apply  -f -
+kind: List
+metadata: {}
+apiVersion: v1
+items:
+  -   apiVersion: project.openshift.io/v1
+      kind: Project
+      metadata:
+          name: redhat-ods-operator
+  -   apiVersion: operators.coreos.com/v1alpha1
+      kind: Subscription
+      metadata:
+          name: rhods-operator
+          namespace: redhat-ods-operator
+      spec:
+          channel: alpha
+          installPlanApproval: Automatic
+          name: rhods-operator
+          source: redhat-operators
+          sourceNamespace: openshift-marketplace
+          startingCSV: rhods-operator.2.2.0
+  -   apiVersion: operators.coreos.com/v1
+      kind: OperatorGroup
+      metadata:
+          name: redhat-operators
+          namespace: redhat-ods-operator
+EOF
+
+oc patch installplan \
+    $(oc get installplans -n redhat-ods-operator | grep -v NAME | awk '{print $1}') \
+    -n redhat-ods-operator \
+    --type='json' \
+    -p '[{"op": "replace", "path": "/spec/approved", "value": true}]'
+
+```
+
+```
+cat <<EOF | oc apply  -f -
+apiVersion: datasciencecluster.opendatahub.io/v1
+kind: DataScienceCluster
+metadata:
+  name: default
+  namespace: redhat-ods-operator
+  labels:
+    app.kubernetes.io/name: datasciencecluster
+    app.kubernetes.io/instance: default
+    app.kubernetes.io/part-of: rhods-operator
+    app.kubernetes.io/managed-by: kustomize
+    app.kubernetes.io/created-by: rhods-operator
+spec:
+  components:
+    codeflare:
+      managementState: Removed
+    dashboard:
+      managementState: Removed
+    datasciencepipelines:
+      managementState: Managed
+    kserve:
+      managementState: Removed
+    modelmeshserving:
+      managementState: Removed
+    ray:
+      managementState: Removed
+    workbenches:
+      managementState: Removed
+EOF
+```
---
+++ training-code/train.py
@@ -0,0 +1,121 @@
+import subprocess
+
+requirements_file = "requirements.txt"
+subprocess.call(["pip", "install", "-r", requirements_file])
+
+import numpy as np
+import pandas as pd
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, BatchNormalization, Activation
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.utils import class_weight
+import tf2onnx
+import onnx
+import pickle
+import os
+from pathlib import Path
+
+Data = pd.read_csv('data/card_transdata.csv')
+# Data.head()
+
+# Set the input (X) and output (Y) data.
+# The only output data we have is if it's fraudulent or not, and all other fields go as inputs to the model.
+
+X = Data.drop(columns = ['repeat_retailer','distance_from_home', 'fraud'])
+y = Data['fraud']
+
+# Split the data into training and testing sets so we have something to test the trained model with.
+
+# X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.2, stratify = y)
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.2, shuffle = False)
+
+X_train, X_val, y_train, y_val = train_test_split(X_train,y_train, test_size = 0.2, stratify = y_train)
+
+# Scale the data to remove mean and have unit variance. This means that the data will be between -1 and 1, which makes it a lot easier for the model to learn than random potentially large values.
+# It is important to only fit the scaler to the training data, otherwise you are leaking information about the global distribution of variables (which is influenced by the test set) into the training set.
+
+scaler = StandardScaler()
+
+X_train = scaler.fit_transform(X_train.values)
+
+Path("artifact").mkdir(parents=True, exist_ok=True)
+with open("artifact/test_data.pkl", "wb") as handle:
+    pickle.dump((X_test, y_test), handle)
+with open("artifact/scaler.pkl", "wb") as handle:
+    pickle.dump(scaler, handle)
+
+# Since the dataset is unbalanced (it has many more non-fraud transactions than fraudulent ones), we set a class weight to weight the few fraudulent transactions higher than the many non-fraud transactions.
+
+class_weights = class_weight.compute_class_weight('balanced',classes = np.unique(y_train),y = y_train)
+class_weights = {i : class_weights[i] for i in range(len(class_weights))}
+
+
+model = Sequential()
+model.add(Dense(32, activation = 'relu', input_dim = len(X.columns)))
+model.add(Dropout(0.2))
+model.add(Dense(32))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Dropout(0.2))
+model.add(Dense(32))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Dropout(0.2))
+model.add(Dense(1, activation = 'sigmoid'))
+model.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
+model.summary()
+
+
+# Train the model and get performance
+
+epochs = 2
+history = model.fit(X_train, y_train, epochs=epochs, \
+                    validation_data=(scaler.transform(X_val.values),y_val), \
+                    verbose = True, class_weight = class_weights)
+
+# Save the model as ONNX for easy use of ModelMesh
+
+model_proto, _ = tf2onnx.convert.from_keras(model)
+os.makedirs("models/fraud", exist_ok=True)
+onnx.save(model_proto, "models/fraud/model.onnx")
+
+
+
+## testing
+from sklearn.metrics import confusion_matrix
+import numpy as np
+import pickle
+import seaborn as sns
+from matplotlib import pyplot as plt
+import onnxruntime as rt
+
+with open('artifact/scaler.pkl', 'rb') as handle:
+    scaler = pickle.load(handle)
+with open('artifact/test_data.pkl', 'rb') as handle:
+    (X_test, y_test) = pickle.load(handle)
+
+sess = rt.InferenceSession("models/fraud/model.onnx", providers=rt.get_available_providers())
+input_name = sess.get_inputs()[0].name
+output_name = sess.get_outputs()[0].name
+y_pred_temp = sess.run([output_name], {input_name: scaler.transform(X_test.values).astype(np.float32)})
+y_pred_temp = np.asarray(np.squeeze(y_pred_temp[0]))
+threshold = 0.995
+y_pred = np.where(y_pred_temp > threshold, 1,0)
+
+
+accuracy = np.sum(np.asarray(y_test) == y_pred) / len(y_pred)
+print("Accuracy: " + str(accuracy))
+
+c_matrix = confusion_matrix(y_test,y_pred)
+ax = sns.heatmap(c_matrix, annot=True, cbar=False, cmap='Blues')
+ax.set_xlabel("Prediction")
+ax.set_ylabel("Actual")
+ax.set_title('Confusion Matrix')
+plt.show()
+
+sally_transaction_details = [[0.3111400080477545, 1.9459399775518593, 1.0, 0.0, 0.0]]
+prediction = sess.run([output_name], {input_name: scaler.transform(sally_transaction_details).astype(np.float32)})
+
+print("Was Sally's transaction predicted to be fraudulent?  ")
+print(np.squeeze(prediction) > threshold)

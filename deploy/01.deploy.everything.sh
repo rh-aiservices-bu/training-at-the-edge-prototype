a1586c62373959a30ab72a12d5ac24a2aa2eb7c7
Erwan Granger <32554862+erwangranger@users.noreply.github.com>
2023-11-08 08:56:56 -0500

    commenting out the safety code

--- deploy/01.deploy.everything.sh
+++ deploy/01.deploy.everything.sh
@@ -3,13 +3,13 @@ NS=${NS:-edgetraining}
 
 oc cluster-info
 
-if oc cluster-info | grep -E -q 'dynamic\.opentlc\.com'
-then
-    printf "OK. Using RHPDS cluster. Continuing\n"
-else
-    printf "Nope. You are not connected to an RHPDS. That is dangerous. Exiting \n"
-    exit 1
-fi
+# if oc cluster-info | grep -E -q 'dynamic\.opentlc\.com'
+# then
+#     printf "OK. Using RHPDS cluster. Continuing\n"
+# else
+#     printf "Nope. You are not connected to an RHPDS. That is dangerous. Exiting \n"
+#     exit 1
+# fi
 
 
 

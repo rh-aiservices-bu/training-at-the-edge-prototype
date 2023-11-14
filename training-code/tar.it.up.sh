679aa80bf5474d8c6cd9d7154cac2e8469e6068c
RHRolun <rlundber@redhat.com>
2023-11-14 13:27:03 +0100

    custom container image for training at the edge

--- training-code/tar.it.up.sh
+++ training-code/tar.it.up.sh
@@ -1,12 +1,8 @@
-
-
-
 rm -f ./pipeline-artifacts/training-artifacts/train-step.tar.gz
 
 tar  -czvf \
     pipeline-artifacts/training-artifacts/train-step.tar.gz \
     card_transdata.csv \
-    requirements.txt \
     train.py
 
 rm -f ./pipeline-artifacts/training-artifacts/upload-step.tar.gz
@@ -14,4 +10,3 @@ rm -f ./pipeline-artifacts/training-artifacts/upload-step.tar.gz
 tar  -czvf \
     pipeline-artifacts/training-artifacts/upload-step.tar.gz \
     upload.py
-
--- training-code/upload.py
+++ training-code/upload.py
@@ -1,13 +1,3 @@
-print("Hello Upload")
-
-import subprocess
-
-requirements_file = "requirements.txt"
-subprocess.call(["pip", "install", "boto3==1.26.165", "botocore==1.29.165"])
-
-# !pip install boto3==1.26.165 \
-#             botocore==1.29.165
-
 import os
 import boto3
 import botocore
